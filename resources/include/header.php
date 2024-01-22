d0340cd6820b5036eba3d8c1b499db7a6d6ef966
diego cesari <82421304+clinton231@users.noreply.github.com>
2024-01-22 14:08:23 -0300

    resourse
    mvc

---
+++ resources/include/header.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?php echo $title; ?></title>
+<!-- icono de la pagina -->
+<link rel="icon" type="image/x-icon" href="../resources/favicon/favicon.ico" />
+<!-- bootstrap css -->
+<link rel="stylesheet" type="text/css" href="../resources/bootstrap-5.3.2/css/bootstrap.min.css">
+<!-- bootstrap icons css -->
+<link rel="stylesheet" type="text/css" href="../resources/bootstrap-icons-1.11.1/bootstrap-icons.min.css">
+<!-- custom css -->
+<link rel="stylesheet" type="text/css" href="../resources/css/styles.css">
